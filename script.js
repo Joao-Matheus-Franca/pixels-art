e86bb07d6a8ed6036236af92a1a4acf33f2db979
João Matheus Silva Franca <matheussfranca@live.com>
2022-07-04 16:44:57 -0300

    Concluido - requisito 06

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ let pixels = document.getElementById('pixel-board').children;
 for (i = 0; i < pixels.length; i += 1) {
     pixels[i].className = 'pixel';
 }
+window.onload = function() {
+    let black = document.getElementById('black');
+    black.classList.add('selected')
+}
