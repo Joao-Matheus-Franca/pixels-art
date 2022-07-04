1f85d4be1a6515868f6079e3e1212e905aef4b8e
João Matheus Silva Franca <matheussfranca@live.com>
2022-07-04 17:34:10 -0300

    Concluido - requisito 07

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
+window.onload = function() {
+    let black = document.getElementById('black');
+    black.classList.add('selected')
+}
+
 let pixels = document.getElementById('pixel-board').children;
 for (i = 0; i < pixels.length; i += 1) {
     pixels[i].className = 'pixel';
 }
-window.onload = function() {
-    let black = document.getElementById('black');
-    black.classList.add('selected')
+
+let black = document.getElementById('black');
+let red = document.getElementById('red');
+let green = document.getElementById('green');
+let blue = document.getElementById('blue');
+
+black.addEventListener('click', addSelected)
+red.addEventListener('click', addSelected)
+green.addEventListener('click', addSelected)
+blue.addEventListener('click', addSelected)
+
+function addSelected (event) {
+    console.log(event.target);
+        document.querySelector('.selected').classList = 'color'
+    if (event.target.classList != 'color selected') {
+        event.target.classList.add('selected')
+    }
 }
